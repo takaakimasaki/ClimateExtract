4303dd0fb0ccc437fd20f61d0bd523174aa1657d
Takaaki Masaki <takaakimasaki@users.noreply.github.com>
2023-04-01 17:45:27 +0200

    added function to download ESRI landcover data

--- R/esri_lc_data_extract.R
+++ R/esri_lc_data_extract.R
@@ -1,12 +1,12 @@
 #' CHIRPS data download.
 #' @param year Year.
-#' @param month Month.
+#' @param utm_id UTM ID.
 #' @param path path in which downloaded data will be stored.
 #' @export
 #' @return `chirps_data_extract()` download all the GEOTIFF files for the period between year_start and year_end from https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_monthly/tifs/.
 #' @examples
 #'\dontrun{
-#' chirps_data_extract(year_start = 20,year_end = 22,month_start=13,month_end=33, region="west_africa", path="./data-clean")
+#' esri_lc_data_extract(year,utm_id,path)
 #'}
 #' @import dplyr stringr
 #' @importFrom raster raster crop mask writeRaster
@@ -15,18 +15,14 @@
 #' @importFrom R.utils gunzip
 
 #path <- "C:/Users/WB495141/OneDrive - WBG/poverty/data/global/chirps"
-chirps_data_extract <- function(year,month,path){
-#y <- 1981
-#m <- "01"
-  url <- paste0("https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_monthly/tifs/chirps-v2.0.",y,".",m,".tif.gz")
+esri_lc_data_extract <- function(year,utm_id,path){
+  #year <- 2017
+  #utm_id <- "36N"
+  year2 <- year+1
+  url <- paste0("https://lulctimeseries.blob.core.windows.net/lulctimeseriespublic/lc",year,"/",utm_id,"_",year,"0101-",year2,"0101.tif")
   fileName <- gsub(".gz","",basename(url))
   #download.file(url_S_C, tmpFile)
   httr::GET(url,httr::write_disk(path=paste0(path,"/",basename(url)), overwrite=TRUE))
-  R.utils::gunzip(paste0(path,"/",basename(url)), overwrite=TRUE)
+  #R.utils::gunzip(paste0(path,"/",basename(url)), overwrite=TRUE)
 }
 
-##check if worked...
-#r <- raster(paste0("C:/Users/WB495141/Downloads/chirps-v2.0.2022.01.tif"))
-#cellStats(r,stat="sum")
-#r <- raster(paste0("C:/Users/WB495141/OneDrive - WBG/poverty/data/global/chirps/chirps-v2.0.2022.01.tif"))
-#cellStats(r,stat="sum")
